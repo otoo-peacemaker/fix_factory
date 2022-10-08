f0f96b7b745a67524bfba1c7c4dfc68f748763eb
Peacemaker Otoo <infofeedacc@gmail.com>
2022-10-08 08:55:01 +0000

    com.android.support:appcompat Migrate to AndroidX
    Version 28 (intended for Android Pie and below) is the last version of the legacy support library, so we recommend that you migrate to AndroidX libraries when using Android Q and moving forward. The IDE can help with this: Refactor > Migrate to AndroidX...

---
+++ supportappcompat.kt
@@ -0,0 +1,12 @@
+//In your build.gradle
+//change
+implementation 'com.android.support:appcompat-v7:27.1.0'
+ //to
+implementation 'androidx.appcompat:appcompat:1.0.0'
+
+
+//in xml file
+//change
+android.support.v7.widget.Toolbar
+//to
+androidx.appcompat.widget.Toolbar
